78d69fb7403528cbe2951b89cfa6eb160d0ce2cb
Karan Shahani <kshahani@premiumbeat.com>
2017-04-20 16:36:39 -0400

    Create a bot which echoes the user's console input

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import builder from 'botbuilder';
+
+let connector = new builder.ConsoleConnector().listen();
+
+let bot = new builder.UniversalBot(connector, (session) => {
+  
+  session.send(`${session.message.user.name} says ${session.message.text}`);
+});
