e84f14d7e67d56713b6b7f057e7e0b8bb33fdc7d
Karan Shahani <kshahani@premiumbeat.com>
2017-04-20 16:40:55 -0400

    Add linting

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-'use strict';
-
 import builder from 'botbuilder';
 
 let connector = new builder.ConsoleConnector().listen();
 
 let bot = new builder.UniversalBot(connector, (session) => {
-  
   session.send(`${session.message.user.name} says ${session.message.text}`);
 });
