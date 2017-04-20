4dcce5332f5a02365af3d2e1bc0867ebe15ad97d
Karan Shahani <kshahani@premiumbeat.com>
2017-04-20 16:58:57 -0400

    Add app ID, password, and name

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import builder from 'botbuilder';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 let connector = new builder.ConsoleConnector().listen();
 
 let bot = new builder.UniversalBot(connector, (session) => {
-  session.send(`${session.message.user.name} says ${session.message.text}`);
+  session.send(`Application ID: ${process.env.APP_ID}`);
+  // session.send(`${session.message.user.name} says ${session.message.text}`);
 });
